80d116d77a08d3a1e05c39d649a1366ce1c64929
mehdihasan <email2mehdihasan@gmail.com>
2023-03-18 20:39:43 +0100

    User tracking consumer

---
+++ apps/user-tracking-consumer/src/main/java/com/test/kafka/consumer/Main.java
@@ -0,0 +1,48 @@
+package com.test.kafka.consumer;
+
+
+import com.test.kafka.model.Product;
+import com.test.kafka.model.User;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+import static java.util.Arrays.asList;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+
+        SuggestionEngine suggestionEngine = new SuggestionEngine();
+
+        Properties props = new Properties();
+        // props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "user-tracking-consumer-v3");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        props.put("value.deserializer", KafkaAvroDeserializer.class.getName());
+        props.put("specific.avro.reader", "true");
+        props.put("schema.registry.url", "http://localhost:8081");
+
+        try (KafkaConsumer<User, Product> consumer = new KafkaConsumer<>(props)) {
+
+            consumer.subscribe(List.of("user-prefer-product-in-avro"));
+
+            while (true) {
+                ConsumerRecords<User, Product> records = consumer.poll(Duration.ofMillis(100));
+
+                for (ConsumerRecord<User, Product> record : records) {
+                    suggestionEngine.processSuggestions(record.key(), record.value());
+                }
+            }
+        }
+    }
+}
